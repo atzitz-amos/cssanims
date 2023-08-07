ba21dfd711d28b40c350a9c2cc297718e0ed70a9
atzitz-amos <amos.atzitz@gmail.com>
2023-08-08 00:33:51 +0200

    Lots of things work

--- htmlEditor/templates/editor.js
+++ htmlEditor/templates/editor.js
@@ -19,7 +19,7 @@ window.onload = () => {
     document.querySelector(".editor-input").addEventListener("input", (e) => {
         writer.writeText(e.data)
         e.target.value = "";
-    });
+    });  // input
 
     document.querySelector(".editor-input").addEventListener("keydown", (e) => {
         switch (e.key) {
@@ -30,26 +30,30 @@ window.onload = () => {
                 writer.writeLine();
                 break;
             case "ArrowLeft":
+                if (e.shiftKey) {selection.moveHoriz(-1);}
                 writer.moveCursorHoriz(-1);
                 break;
             case "ArrowRight":
+                if (e.shiftKey) {selection.moveHoriz(1);}
                 writer.moveCursorHoriz(1);
                 break;
             case "ArrowUp":
+                if (e.shiftKey) {selection.moveVert(-1);}
                 writer.moveCursorVert(-1);
                 break;
             case "ArrowDown":
+                if (e.shiftKey) {selection.moveVert(-1);}
                 writer.moveCursorVert(1);
                 break;
         }
-    });
+    });  // keys
 
     document.querySelector(".tab-editor").addEventListener("click", (e) => {
         var rect = document.querySelector("#line-1").getBoundingClientRect();
         var x = e.x - rect.x; var y = e.y - rect.y;
         const line = Math.ceil(y / 20);
         writer.moveCursorTo(x, line);
-    });
+    });  // pointer
 };
 
 
@@ -87,12 +91,41 @@ class Line {
     deleteLine () {
         document.querySelector(".editor-inner").removeChild(this.element);
     }
+
+    static updateLineNumbersUp(index, lines, lineN){
+        var lineC = document.querySelector(".tab-lines");
+        var toAddBefore = null;
+        lineC.childNodes.forEach((el, i) => {
+            if (i == index - 1) {toAddBefore = el;}
+            if(i >= index - 1) {
+                el.innerHTML = i + 2;
+                lines[i].element.id = "line-" + (i + 2);
+                lines[i].number = i + 2;
+            }
+        });
+        lineC.insertBefore(lineN, toAddBefore);
+    }
+
+    static updateLineNumbersDown(index, lines) {
+        var lineC = document.querySelector(".tab-lines");
+        var toBeRemoved = null;
+        lineC.childNodes.forEach((num, i) => {
+            if(i == index - 1) {toBeRemoved=num;}
+            if(i > index - 1) {
+                num.innerHTML = i;
+                lines[i].element.id = "line-" + i;
+                lines[i].number = i;
+            }
+        });
+        lineC.removeChild(toBeRemoved);
+    }
 }
 
 cursor = {
     x: 0,
     y: 0,
     textPos: 0,
+    vertMovementPos: null,
     blinkID: null,
     init: function () {this.startBlink();},
     startBlink: function () {this.blinkID = window.setInterval(()=>{switchAttr(document.querySelector(".tab-editor-cursor"), "hidden");}, 750);},
@@ -119,8 +152,9 @@ cursor = {
         sizer.innerHTML = text;
         return parseInt(window.getComputedStyle(sizer).width.slice(0, -2));
     },
-    changeLine: function (text, direction=-1) {
-        this.setPos(this.getWidth(text), this.y + 20 * direction);
+    changeLine: function (textA, textB, direction=-1) {
+        this.setPos(this.getWidth(textB), this.y + 20 * direction);
+        this.textPos = textB.length;
     },
     moveApproxX: function (approxX, text) {
         var minDistance = null;
@@ -141,6 +175,9 @@ cursor = {
         this.stopBlink();
         this.startBlink();
     },
+    setVertMovementPos(newPos){
+        this.vertMovementPos = Math.max(this.vertMovementPos, newPos);
+    }
 }
 
 writer = {
@@ -151,26 +188,35 @@ writer = {
     },
     writeText: function (text) {cursor.setText(this.lines[this.currentline - 1].write(text, cursor.textPos));},
     writeLine: function () {
-        // TODO: implement writeLine() when cursor not at end of line
-        this.lines[this.currentline - 1].setCurrent(false);
-        if (this.lines.length > this.currentline){
-            this.currentline++;
+        var prevline = this.lines[this.currentline - 1]
+        prevline.setCurrent(false);
+        var toMove = "";
+        if (cursor.textPos < prevline.text().length) {
+            toMove = prevline.text().slice(cursor.textPos);
+            prevline.element.innerHTML = prevline.text().slice(0, cursor.textPos);
         }
-        else{
-            this.currentline++;
-            line = document.createElement("div");
-            line.className = "line current-line";
-            line.id = "line-" + this.currentline;
-            document.querySelector(".editor-inner").appendChild(line);
-            linenumber = document.createElement("div");
-            linenumber.className = "tab-line-number";
-            linenumber.innerHTML = this.currentline;
-            if(this.currentline >= 10) {linenumber.style.marginRight = "2px";}
-            document.querySelector(".tab-lines").appendChild(linenumber);
-            this.lines.push(new Line(this.currentline, line, true));
-        }
-        cursor.changeLine(this.lines[this.currentline - 1].text(), direction=1);
-        this.lines[this.currentline - 1].setCurrent(true);
+        this.insertLine(++this.currentline);
+        var line = this.lines[this.currentline - 1];
+        line.setCurrent(true);
+        line.append(toMove);
+        cursor.changeLine(this.lines[this.currentline - 2].text(), "", direction=1);
+    },
+    insertLine: function (index) {
+        line = document.createElement("div");
+        line.className = "line";
+        line.id = "line-" + index;
+        var container = document.querySelector(".editor-inner");
+        if (index == container.childNodes.length-1) {container.appendChild(line);}
+        else {container.insertBefore(line, this.lines[index-1].element);}
+        /* LINE NUMBER */
+        lineN = document.createElement("div");
+        lineN.className = "tab-line-number";
+        lineN.innerHTML = index;
+        if(this.currentline >= 10) {lineN.style.marginRight = "2px";}
+        var lineC = document.querySelector(".tab-lines");
+        if(index == lineC.childNodes.length + 1) {lineC.appendChild(lineN);}
+        else {Line.updateLineNumbersUp(index, this.lines, lineN);}
+        this.lines.splice(index-1, 0, new Line(this.currentline, line, true));
     },
     deletePrevChar: function () {
         if(this.lines[this.currentline - 1].empty()) return this.deleteLine();
@@ -187,21 +233,18 @@ writer = {
     deleteLine: function () {
         if(this.currentline == 1) return;
         this.lines[this.currentline - 1].deleteLine();
-        this.lines.splice(this.lines.indexOf(this.lines[this.currentline - 1]), 1);
         // line number
         var lnums = document.querySelector(".tab-lines");
         var toBeRemoved = null;
-        lnums.childNodes.forEach((num, index) => {
-            if(index == this.currentline - 1) {toBeRemoved=num;}
-            if(index > this.currentline - 1) {num.innerHTML = parseInt(num.innerHTML) - 1;}
-        });
-        lnums.removeChild(toBeRemoved);
+        Line.updateLineNumbersDown(this.currentline, this.lines);
         this.currentline--;
-        cursor.changeLine(this.lines[this.currentline - 1].text());
+        cursor.changeLine(this.lines[this.currentline].text(), this.lines[this.currentline - 1].text());
         this.lines[this.currentline - 1].setCurrent(true);
+
+        this.lines.splice(this.lines.indexOf(this.lines[this.currentline]), 1);
     },
     moveCursorTo: function (x, line) {
-        // TODO: Fix bug that occurs when one click on the middle of a letter
+        // TODO [delayed fix]: Fix bug that occurs when one click on the middle of a letter
         if (line > this.lines.length){line = this.lines.length;}
         if (line < 1) {line = 1;}
         this.lines[this.currentline - 1].setCurrent(false);
@@ -212,19 +255,57 @@ writer = {
     },
     moveCursorVert: function (direction) {
         if (this.currentline + direction == 0 || this.currentline + direction > this.lines.length) return;
-        this.lines[this.currentline - 1].setCurrent(false);
+        var firstline = this.lines[this.currentline - 1];
+
+        firstline.setCurrent(false);
         this.currentline += direction;
         this.lines[this.currentline - 1].setCurrent(true);
-        cursor.changeLine(this.lines[this.currentline - 1].text(), direction=direction);
+
+        cursor.setVertMovementPos(cursor.textPos);
+        textB = this.lines[this.currentline - 1].text().substring(0, cursor.vertMovementPos);
+        cursor.setVertMovementPos(textB.length);
+        cursor.changeLine(firstline.text(), textB, direction=direction);
     },
     moveCursorHoriz: function (direction) {
         var line = this.lines[this.currentline - 1];
         if (cursor.textPos + direction < 0){
-            return this.moveCursorVert(-1);
+            this.moveCursorVert(-1);
+            cursor.setText(this.lines[this.currentline - 1].text());
         }
         else if (cursor.textPos + direction > line.text().length){
-            return this.moveCursorVert(1);
+            this.moveCursorVert(1);
+            cursor.setText("");
         }
-        cursor.setText(line.text().slice(0, cursor.textPos + direction));
+        else {cursor.setText(line.text().slice(0, cursor.textPos + direction));}
+        cursor.vertMovementPos = null;
+    }
+}
+
+selection = {
+    selectionStartX: null,
+    selectionStartY: null,
+    selectionActive: false,
+
+    startEl: function () {return document.querySelector("selection-start");}
+    middleEl: function () {return document.querySelector("selection-middle");}
+    endEl: function () {return document.querySelector("selection-end");}
+
+    start: function (x, y) {
+        this.selectionStartX = x;
+        this.selectionStartY = y;
+        this.selectionActive = true;
+        this.startEl().style.left = x;
+        this.startEl().style.top = y;
+    },
+    moveHoriz: function (direction) {
+        if (!selectionActive) {
+            start(cursor.x, cursor.y);
+        }
+        this.expand(direction);
+    },
+    expand: function (direction) {
+        if (direction == -1){
+
+        }
     }
 }
